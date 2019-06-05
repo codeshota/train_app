b9317dfccbf2ba8ce7cbf21500e9730b5f347a71
GrigoryCode <grigorycode@gmail.com>
2019-06-05 12:08:40 +0300

    Trains and station association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_195942) do
+ActiveRecord::Schema.define(version: 2019_06_05_082905) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_195942) do
     t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "current_station_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
   end
 
 end
